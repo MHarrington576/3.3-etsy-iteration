0a7d2812a2940cc98808d3fb01968d575496b489
Matthew Harrington <mattharrington576@gmail.com>
2016-09-30 12:19:29 -0400

    Project complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*---------------------Question 1------------------------*/
 (function(){
   "use strict";
 
@@ -5,6 +6,85 @@
     return total + item.price;
   }, 0);
 
-  console.log('$' + (itemTotal/items.length).toFixed(2));
+  console.log('Q1) ' + '$' + (itemTotal/items.length).toFixed(2));
+  console.log('');
+
+}());
+
+
+/*---------------------Question 2------------------------*/
+(function(){
+  "use strict";
+
+  var cheapProducts = items.map(function(item){
+    if(item.price >= 14 && item.price <= 18){
+      console.log('Q2) ' + item.title + ': ' + '$' + item.price);
+      return (item.title + ': ' + '$' + item.price);
+    }
+  });
+  console.log('');
+
+}());
+
+
+/*---------------------Question 3------------------------*/
+(function(){
+  "use strict";
+
+  var britishProduct = items.filter(function(item){
+    if(item.currency_code == "GBP"){
+      console.log('Q3) ' + item.title + ': ' + '£' + item.price);
+      return (item.title + ': ' + '£' + item.price);
+    }
+  });
+  console.log('');
+
+}());
+
+
+/*---------------------Question 4------------------------*/
+(function(){
+  "use strict";
+
+  var woodProducts = items.map(function(item){
+    if(item.materials.indexOf('wood') !== -1){
+      console.log('Q4) Wooden: ' + item.title);
+      return item.title;
+    }
+  });
+  console.log('');
+
+}());
+
+
+/*---------------------Question 5------------------------*/
+(function(){
+  "use strict";
+
+  var complexItems = items.map(function(item){
+    if(item.materials.length >= 8){
+      console.log('Q5) ' + item.title + ' ~ ' + item.materials.length + ' materials');
+      return item.title;
+    }
+  });
+  console.log('');
+
+}());
+
+
+/*---------------------Question 6------------------------*/
+(function(){
+  "use strict";
+
+  var homemadeItemCount = 0;
+  var homemadeItems = items.map(function(item){
+    if(item.who_made == "i_did"){
+      homemadeItemCount += 1;
+    }
+    return homemadeItemCount;
+  });
+
+  var finalCount = homemadeItemCount;
+  console.log('Q6) ' + finalCount + ' items');
 
 }());
